97eb64e675162ba0822d7fc80336f53ce20aa4cd
alexis romero mendoza <alexis.rm162917@gmail.com>
2024-02-23 14:41:57 -0700

    adding media queries

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,38 @@
 import './NavBarStyles.css';
 import cv from '../files/alexis_romero_mendoza_CV.pdf';
-
 export const NavBar = () => {
+
+  
+
   return (
-    <ul>
-      <li>
-        <a href='#homePage' className='navItem'>Home</a>
-      </li>
-      <li>
-        <a href={cv} target='_blank' className='navItem'>
-          Download CV
-        </a>
-      </li>
-      <li>
-        <a href='#projectsPage' className='navItem'>
-          Projects
-        </a>
-      </li>
-      <li>
-        <a href='#aboutPage' className='navItem'>
-          About me
-        </a>
-      </li>
-      <li>
-        <a href='#contactPage' className='navItem'>
-          Contact
-        </a>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <a href='#homePage' className='navItem'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href={cv} target='_blank' className='navItem'>
+            Download CV
+          </a>
+        </li>
+        <li>
+          <a href='#projectsPage' className='navItem'>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href='#aboutPage' className='navItem'>
+            About me
+          </a>
+        </li>
+        <li>
+          <a href='#contactPage' className='navItem'>
+            Contact
+          </a>
+        </li>
+      </ul>      
+    </nav>
   );
 };
